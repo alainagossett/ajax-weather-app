563cc8252a90df87ed53f96abf5d9cfc9d422879
Alaina <alaina.gossett@gmail.com>
2021-10-23 13:20:46 -0600

    wip: completes functions logic

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
+//IIFE FUNCTION
+$(function () {
 
-//CONSTANTS
-BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?q='
-API_KEY = '043c3f8d863dc027ae353d81dbf2dfd3'
-
-//VARIABLES
-
-
-//CACHED ELEMENT REFERENCES
-const $weatherFor = $('#weatherFor');
-const $temperature = $('#temperature');
-const $feelsLike = $('#feelsLike');
-const $weather = $('#weather');
-const $input = $('input[type="text"]')
-
-//EVENT LISTENERS
-$form.on('submit', handleSubmit);
-
-
-//FUNCTIONS
-
-function handleSubmit(evt) {
-    evt.preventDefault();
-
-    const currWeather = $input.val();
-    console.log(currWeather);
-
-
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-$.ajax(`${BASE_URL}${input}&appid=${API_KEY}`)
-    .then(function (data) {
-        
+    // Constants
+    BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    API_KEY = 'c766f08b64c511e7c386ea68613046ac';
+  
+    // Cached Element References
+    const $input = $('input[type="text"]')
+    const $main = $('main');
+    const $form = $('form')
+  
+  
+    // vars 
+    let weatherData;
+  
+    // Event Listener
+    $form.on('submit', handleSubmit);
+  
+    // Functions
+  
+    function handleSubmit(evt) {
+      evt.preventDefault();
+      const currCity = $input.val();
+      console.log(currCity)
+      $.ajax(`${BASE_URL}${currCity}&appid=${API_KEY}`)
+        .then(function(data) {
+          weatherData = data
         render();
-    }, function (error) {
-        console.log(error);
-    });
-
-}
-
-function render() {
-    
-}
+      }, function (error){
+        console.log("error:", error)
+      })
+  }
+        
+    // add data as text content to our DOM elements
+    function render () {
+      $main.html(
+        // create p tags with weatherData values interpolated within them
+        `
+        <p>Weather For: ${weatherData.name}</p>
+        <p>Temperature: ${weatherData.main.temp}</p>
+        <p>Feels Like: ${weatherData.main.feels_like}</p>
+        <p>Weather: ${weatherData.weather[0].description}</p>
+        `
+      )
+    }
+  
+   })
+  
